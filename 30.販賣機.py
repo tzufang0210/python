782fae2af993bd479733fc20bd927328d90755b9
tzufang0210 <82918754+tzufang0210@users.noreply.github.com>
2021-05-09 23:22:18 +0800

    Delete 30.販賣機.py

--- 30.販賣機.py
+++
@@ -1,9 +0,0 @@
-mon=int(input("小名身上有多少錢:"))
-can=int(input("販賣機有幾種飲料:"))
-print("",end="")
-count=0
-for x in range(0,can):
-    sell=int(input("\n"))
-    if sell<=mon:
-        count+=1
-print(count)
